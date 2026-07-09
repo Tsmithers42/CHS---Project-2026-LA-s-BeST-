5a579091ffbe2b5066933fe367529c5c42031b4d
Tsmithers42 <tsmith324@rams.wssu.edu>
2026-07-09 10:59:30 -0700

    Add files via upload
    All test ran on FEV now ran on FVC + notes

---
+++ Key testing notes.R
@@ -0,0 +1,30 @@
+## After running several test of the pollutants with the co factors (male, age, height) 
+##and without them, I have noticed PM2.5 is the only pollutant that Shows its statistically significant,
+##with its p-value being the only one that meets the 5% mark meaning that the others could be
+##showing more significant just in the presence of other pollutants Or just by chance.
+
+## I then did just the pollutants with FEV as outcome and it shows sulfate being the largest negitive predictor
+##and nitrate being the largest positive predictor, while showing sulfate as the strongest.
+
+## So, from there I did FEV vs Sulfate by male, since they were my "strongest" predictors.
+## Although, that was just the outcome when all pollutants were together, alone PM2.5 stands as
+##the only statistically significant pollutant.
+## Since PM2.5 is the strongest alone I did FEV vs PM2.5 by male, again using the 2 "strongest"
+##pollutants. My results for both, graph wise, was similar. The both had clusters of dots
+##but also a slightly downward slope line which makes since because as units of the pollutants 
+##increase levels of FEV decrease.
+
+
+## I will now run the same test for FVC to see if the factor (pollutants & co factors)
+##affect the lung capacity (FVC)  the same it affect lung function (FEV).
+
+## After running the same text i ran for FEV, on FVC i was able to see the that the share a predictor
+##both FEV and FVC are impacted by sulfate (with it being a statistically significant pollutant).
+## Male is also FEV/FVC strongest co factor while, pm2.5 when alone affects FEV more than in presents of other pollutants.
+## So PM2.5 would most likely be FEV strongest predictor & Sulfate is the only significant predictor in FVC.
+
+## Now that ive run test for both i went back and did FEV vs PM2.5 by male since it was its largest pollutant alone.
+## Now i have ran a test comparing sulfate by male to both outcomes (FEV, FVC),
+##i have seen sulfate has a similar affect on both FEV & FVC, as does male but females
+##have have more of an negative connection to FVC in presence of sulfate pollutant.
+
